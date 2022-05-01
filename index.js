7371f833272e5842929c9cf433bbe7a8b0e4488b
jaedonvisva <jaedon.visva2006@gmail.com>
2022-05-01 18:08:50 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+//nav bar
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
