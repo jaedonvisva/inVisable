66fa6e498f938168b79c15d3ec37d4680384107f
jaedonvisva <jaedon.visva2006@gmail.com>
2022-05-06 19:13:26 -0400

    about us

--- index.js
+++
@@ -1,8 +0,0 @@
-//nav bar
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-}
-
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-}
